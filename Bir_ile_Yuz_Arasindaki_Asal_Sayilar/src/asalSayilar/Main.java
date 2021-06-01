d3e5838d6c3456df49b5fe728094b09a13e8a121
mazlumabul <mazlumabul10@gmail.com>
2021-06-02 02:49:05 +0300

    Java ile 1 - 100 arasındaki asal sayıları ekrana yazdıran programı yazınız.

---
+++ Bir_ile_Yuz_Arasindaki_Asal_Sayilar/src/asalSayilar/Main.java
@@ -0,0 +1,24 @@
+package asalSayilar;
+
+//Java ile 1 - 100 arasýndaki asal sayýlarý ekrana yazdýran programý yazýnýz.
+public class Main {
+	public static void main(String[] args) {
+		int sayac = 0;
+		
+		for (int sayi = 2; sayi <= 100; sayi++) {
+			int kontrol = 0;
+			
+			for (int i = 2; i < sayi; i++) {
+				if (sayi % i == 0) {
+					kontrol = 1;
+					break;
+				}
+			}
+
+			if (kontrol == 0) {
+				System.out.print(sayi + " ");
+				sayac++;
+			}
+		}
+	}
+}
