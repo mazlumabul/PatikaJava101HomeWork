4834b75de6ececcf76111f6dceccc7b4963ad9d9
mazlumabul <mazlumabul10@gmail.com>
2021-06-10 18:21:46 +0300

    Java dilinde, dizideki elemanları küçükten büyüğe sıralayan programı yazınız. Dizinin boyutunu ve dizinin elemanlarını kullanıcıdan alınız.

---
+++ Dizi_Elemanlari_Sirala/src/sirala/Main.java
@@ -0,0 +1,27 @@
+package sirala;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Dizi Boyutu : ");
+		int diziBoyutu = scanner.nextInt();
+
+		int[] list = new int[diziBoyutu];
+
+		for (int i = 0; i < list.length; i++) {
+
+			System.out.print((i + 1) + ". eleman : ");
+			list[i] = scanner.nextInt();
+		}
+
+		Arrays.sort(list);
+
+		for (int i = list.length; i > 0; --i) {
+			System.out.println(list[i - 1]);
+		}
+	}
+}
